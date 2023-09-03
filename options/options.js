0bc1de08363ff4a96a79ad06dec875737807b223
Crowdbotics <team@crowdbotics.com>
2023-09-03 21:16:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jjs-life-of-war-43220",
+  url: "https://jjs-life-of-war-43220.botics.co",
+  api: "https://jjs-life-of-war-43220.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
